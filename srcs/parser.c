6adb078c252ee0a438fb95461812f68c1a6b849d
lavrenovamaria <wrickard@student.21-school.ru>
2022-02-20 20:17:52 +0300

    wrong nbr of args

--- srcs/parser.c
+++ srcs/parser.c
@@ -10,6 +10,25 @@ void	*ft_memset(void *str, int ch, size_t size_buff)
 	return (str);
 }
 
+size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (src[i] != '\0' && (i + 1) < dstsize)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (dstsize)
+		dest[i] = '\0';
+	while (src[j] != '\0')
+		j++;
+	return (j);
+}
+
 void	ft_bzero(void *dst, size_t num_bytes)
 {
 	ft_memset(dst, 0, num_bytes);
@@ -45,8 +64,93 @@ int	ft_strcmp(const char *s1, const char *s2)
 	return (s1[i] - s2[i]);
 }
 
+int	ft_strncmp(const char *str1, const char *str2, size_t count_size)
+{
+	size_t	i;
 
+	i = 0;
+	if (count_size == 0)
+		return (0);
+	while (*str1 != '\0' && (*str1 == *str2) && (i < count_size - 1))
+	{
+		str1++;
+		str2++;
+		count_size--;
+	}
+	return ((unsigned char)*str1 - *str2);
+}
 
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, size * count);
+	return (ptr);
+}
+
+char	*ft_strdup(const char *src)
+{
+	char	*dst;
+	int		i;
+
+	i = 0;
+	while (*(src + i))
+		i++;
+	dst = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (dst == NULL)
+		return (NULL);
+	while (i > 0)
+	{
+		*(dst + i) = *(src + i);
+		i--;
+	}
+	*dst = *src;
+	return (dst + i);
+}
+
+char	*ft_strjoin(char const *str1, char const *str2)
+{
+	char	*res;
+	size_t	str_len1;
+	size_t	str_len2;
+
+	if (!str1 || !str2)
+		return (NULL);
+	str_len1 = ft_strlen(str1);
+	str_len2 = ft_strlen(str2);
+	res = (char *)malloc(sizeof(char) * (str_len1 + str_len2 + 1));
+	if (res == NULL)
+		return (NULL);
+	ft_strlcpy(res, str1, (str_len1 + 1));
+	ft_strlcat(res, str2, (str_len1 + str_len2 + 1));
+	return (res);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	s_len;
+	size_t	d_len;
+	size_t	offset;
+	size_t	src_i;
+
+	s_len = ft_strlen(src);
+	d_len = ft_strlen(dst);
+	offset = d_len;
+	src_i = 0;
+	if (size < d_len)
+		return (size + s_len);
+	while ((*(src + src_i)) && (src_i + d_len + 1 < size))
+	{
+		*(dst + offset) = (src[src_i]);
+		src_i++;
+		offset++;
+	}
+	*(dst + offset) = '\0';
+	return (d_len + s_len);
+}
 	//const char *ext = ".jpeg";
 	//size_t xlen = strlen(ext);
 	//size_t slen = strlen(argv[1]);
@@ -55,52 +159,97 @@ int	ft_strcmp(const char *s1, const char *s2)
 /////////////////////////
 
 
-// void print_data(t_cub *cub)
-// {
-// 	t_map *tmp;
-// 	printf("________________________________________\n");
-// 	if (cub == NULL)
-// 	{
-// 		printf("Data struct not allocated\n");
-// 		printf("________________________________________\n");
-// 		return ;
-// 	}
-// 	if (cub->NO != NULL)
-// 		printf("NO : %s\n", cub->NO);
-// 	else
-// 		printf("NO : NULL\n");
-// 	if (cub->SO != NULL)
-// 		printf("SO : %s\n", cub->SO);
-// 	else
-// 		printf("SO : NULL\n");
-// 	if (cub->WE != NULL)
-// 		printf("WE : %s\n", cub->WE);
-// 	else
-// 		printf("WE : NULL\n");
-// 	if (cub->EA != NULL)
-// 		printf("EA : %s\n", cub->EA);
-// 	else
-// 		printf("EA : NULL\n");
-// 	printf("Floor RGB : %d %d %d\n", cub->F[0], cub->F[1], cub->F[2]);
-// 	printf("Ceiling RGB : %d %d %d\n", cub->C[0], cub->C[1], cub->C[2]);
-// 	if (cub->id_done == true)
-// 		printf("ID done and valid? : Yes\n");
-// 	else
-// 		printf("ID done and valid? : No\n");
-// 	if (cub->map_done == true)
-// 		printf("Map done and valid? : Yes\n");
-// 	else
-// 		printf("Map done and valid? : No\n");
-// 	tmp = cub->map;
-// 	if (tmp == NULL)
-// 		printf("Nothing stored in map\n");
-// 	while(tmp != NULL)
-// 	{
-// 		printf("x = %d\t y = %d\t o = %c\n", tmp->x, tmp->y, tmp->o);
-// 		tmp = tmp->next;
-// 	}
-// 	printf("________________________________________\n");
-// }
+void print_data(t_data *data)
+{
+	t_map *tmp;
+	printf("________________________________________\n");
+	if (data == NULL)
+	{
+		printf("Data struct not allocated\n");
+		printf("________________________________________\n");
+		return ;
+	}
+	if (data->NO != NULL)
+		printf("NO : %s\n", data->NO);
+	else
+		printf("NO : NULL\n");
+	if (data->SO != NULL)
+		printf("SO : %s\n", data->SO);
+	else
+		printf("SO : NULL\n");
+	if (data->WE != NULL)
+		printf("WE : %s\n", data->WE);
+	else
+		printf("WE : NULL\n");
+	if (data->EA != NULL)
+		printf("EA : %s\n", data->EA);
+	else
+		printf("EA : NULL\n");
+	printf("Floor RGB : %d %d %d\n", data->F[0], data->F[1], data->F[2]);
+	printf("Ceiling RGB : %d %d %d\n", data->C[0], data->C[1], data->C[2]);
+	// if (cub->id_done == true)
+	// 	printf("ID done and valid? : Yes\n");
+	// else
+	// 	printf("ID done and valid? : No\n");
+	// if (cub->map_done == true)
+	// 	printf("Map done and valid? : Yes\n");
+	// else
+	// 	printf("Map done and valid? : No\n");
+	// tmp = data->map;
+	// if (tmp == NULL)
+	// 	printf("Nothing stored in map\n");
+	// while(tmp != NULL)
+	// {
+	// 	printf("x = %d\t y = %d\t o = %c\n", tmp->x, tmp->y, tmp->o);
+	// }
+	printf("________________________________________\n");
+}
+
+
+void	error_exit(char *msg)
+{
+	printf("Error\n");
+	printf("%s\n", msg);
+	exit(-1);
+}
+
+void	*var_malloc(size_t size)
+{
+	void	*var;
+
+	var = ft_calloc(1, size);
+	if (!var)
+		error_exit("malloc error");
+	return (var);
+}
+
+int		overlap_error(t_data *data, int type)
+{
+	if (data->map->info_check[type] == type)
+		error_exit("overlap error");
+	else
+		data->map->info_check[type] = type;
+	return (type);
+}
+
+int		check_gnl(t_data *data)
+{
+	if (!ft_strncmp(data->line, "NO ", 3))
+		return (overlap_error(data, NORTH));
+	else if (!ft_strncmp(data->line, "SO ", 3))
+		return (overlap_error(data, SOUTH));
+	else if (!ft_strncmp(data->line, "WE ", 3))
+		return (overlap_error(data, WEST));
+	else if (!ft_strncmp(data->line, "EA ", 3))
+		return (overlap_error(data, EAST));
+	else if (!ft_strncmp(data->line, "F ", 2))
+		return (overlap_error(data, FLOOR_COLOR));
+	else if (!ft_strncmp(data->line, "C ", 2))
+		return (overlap_error(data, CEILING_COLOR));
+	else
+		error_exit("invalid map info");
+	return (-1);
+}
 
 int		isformat(char *s1, char *s2)
 {
@@ -139,6 +288,22 @@ void	exit_failure(t_data *data, char *error)
 	exit(0);
 }
 
+// void	split_map_get_rows(t_data *data, char *map)
+// {
+// 	int	i;
+
+// 	i = -1;
+// 	data->map->rows = (int*)var_malloc(sizeof(int) * data->map->cols);
+// 	data->map = ft_split(map, '\n');
+// 	while (++i < info->cols)
+// 		data->map->rows[i] = (int)ft_strlen(data->map[i]);
+// 	i = -1;
+// 	data->map->map_mask = (int**)var_malloc(sizeof(int*) * data->map->cols);
+// 	while (++i < data->map->cols)
+// 		info->map_mask[i] = (int*)var_malloc(sizeof(int) * data->map->rows[i]);
+// 	free(map);
+// }
+
 t_data *parser(t_data *data, char *filename)
 {
 	char *one_line;
@@ -146,28 +311,27 @@ t_data *parser(t_data *data, char *filename)
 	char *tmp_map;
 	char *map = 0;
 
-	if(data->fd = open(filename, O_RDONLY) < 0 || !isformat(filename, ".cub"))
+	if((data->fd = open(filename, O_RDONLY)) < 0 || !isformat(filename, ".cub"))
 		exit_failure(data, "Sorry, filename could not be found or extension is not valid");
 	ft_bzero(&data, sizeof(t_data));
 	init_data(data);
-	while (get_next_line(data->fd, data->line))
+	while (get_next_line(data->fd, &data->line))
 	{
-		if (*one_line != '\0' && ++i <= CEILING_COLOR)
-			data->map->info_map[check_gnl(data, one_line)] = ft_strdup(one_line);
-		else if (*one_line != '\0')
+		if (data->line != '\0' && ++i <= CEILING_COLOR)
+			data->map->info_map[check_gnl(data)] = ft_strdup(data->line);
+		else if (data->line != '\0')
 		{
 			tmp_map = map;
 			if (!map)
-				map = ft_strdup(one_line);
+				map = ft_strdup(data->line);
 			else
-				map = ft_strjoin(map, one_line);
+				map = ft_strjoin(map, data->line);
 			free(tmp_map);
 			data->map->cols++;
 		}
-		free(one_line);
 		free(data->line);
-		print_data(data->line);
-		split_map_get_rows(data, map);
+		//split_map_get_rows(data, map);
+		print_data(data);
 	}
 	return(data);
 }
